d4566908e1f107baed662e43ce66b47313fd784b
Sahil Bajaj <sahil29@gmail.com>
2013-06-04 03:59:36 +0200

    Change pager and set less options

--- config.fish
+++ config.fish
@@ -9,10 +9,14 @@ rbenv rehash >/dev/null ^&1
 # Include aliases
 . $HOME/dotfiles/aliases
 
-# Set colors
+# Set default pager and options for less
+# (copied from zsh, makes it scroll faster for some reason)
+set -x PAGER less
+set -x LESS '-F -g -i -M -R -S -w -X -z-4'
 
+# Set colors
 set fish_color_command 89adff
-set fish_color_cwd ffffa4 
+set fish_color_cwd ffffa4
 set fish_color_host 8cd6ff
 set fish_color_user fdd8aa
 
