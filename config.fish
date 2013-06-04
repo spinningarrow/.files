7fbb34b45f1a88b6cb426ee62848b65f4d8dc26b
Sahil Bajaj <sahil29@gmail.com>
2013-06-04 03:55:52 +0200

    Initialise rbenv

--- config.fish
+++ config.fish
@@ -1,6 +1,10 @@
 # Update path
 
 set -x PATH $PATH /usr/local/share/npm/bin /usr/local/share/python /usr/local/bin /usr/local/sbin $HOME/bin
+# Initialise rbenv
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
 
 # Include aliases
 . $HOME/dotfiles/aliases
