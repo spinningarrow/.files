424dd5e94ee6ffdbf9009c66753d003c998bbf2f
Sahil Bajaj <sahil29@gmail.com>
2014-11-16 23:59:38 +0800

    [install] Add ssh config to install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@
 DOTFILES="$HOME/.files"
 OS="osx"
 
-# Symlink files
+# Symlink general files
 ln -s "$DOTFILES/.gitconfig" $HOME/
 ln -s "$DOTFILES/.gitignore" $HOME/
 ln -s "$DOTFILES/.tmux.conf" $HOME/
+ln -s "$DOTFILES/.ssh_config" $HOME/.ssh/config
 
 # Fish config
 ln -s "$DOTFILES/fish/config.fish.$OS" $HOME/.config/fish/config.fish
