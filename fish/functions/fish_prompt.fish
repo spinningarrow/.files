d83510dec9a10a2cf2695342ef7503769c8902b5
Sahil Bajaj <sahil29@gmail.com>
2015-07-28 23:01:59 +0800

    [fish] Use custom colors in fish prompt
    - I like those colors

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,12 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+  # Set custom color variables
+  set -l username_color fdd8aa
+  set -l prompt_color 8cd6ff
+
+  set -l last_status $status
 
   # User
-  set_color $fish_color_quote
+  set_color $username_color
   echo -n (whoami)
   set_color normal
 
@@ -10,18 +14,16 @@ function fish_prompt --description 'Write out the prompt'
 
   # PWD
   set_color $fish_color_cwd
-  # echo -n (prompt_pwd)
-  echo -n (pwd | sed -e "s|$HOME|~|")
+  echo -n (pwd | sed -e "s|$HOME|~|") # echo -n (prompt_pwd)
   set_color normal
 
-  # __terlar_git_prompt
-  __fish_git_prompt
+  __fish_git_prompt # __terlar_git_prompt
   echo
 
   if not test $last_status -eq 0
     set_color $fish_color_error
   else
-    set_color blue
+    set_color $prompt_color
   end
 
   echo -n '∫ '
