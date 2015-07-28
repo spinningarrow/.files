6eecbf2e365a8f812d4ca0bd6fbb96c560ed4724
Sahil Bajaj <sahil29@gmail.com>
2015-07-28 18:16:01 +0800

    [fish] Update fish prompt colors to prevent set_color errors
    - Need to check this on OS X and see why the error doesn't happen there
      - use standard colors if possible

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
 
   # User
-  set_color $fish_color_user
+  set_color $fish_color_quote
   echo -n (whoami)
   set_color normal
 
@@ -21,7 +21,7 @@ function fish_prompt --description 'Write out the prompt'
   if not test $last_status -eq 0
     set_color $fish_color_error
   else
-    set_color $fish_color_host
+    set_color blue
   end
 
   echo -n '∫ '
