0508d174f324984893aa91d2539582d342282619
Sahil Bajaj <sahil29@gmail.com>
2016-07-11 20:27:09 +0800

    [fish] Removed unused stuff

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -1,6 +1,5 @@
-abbr -a - cd -
+abbr -a - 'cd -'
 abbr -a axel 'axel -a'
-abbr -a c clear
 abbr -a dl 'wget -r -l 1 -np -nd'
 abbr -a fn 'find . -iname'
 abbr -a g git
@@ -9,4 +8,3 @@ abbr -a lx 'less -X'
 abbr -a q exit
 abbr -a ql 'qlmanage -p'
 abbr -a v vim
-abbr -a week 'date +%V'
--- fish/.config/fish/functions/pinknoise.fish
+++
@@ -1,3 +0,0 @@
-function pinknoise --description 'Generate pink noise'
-	play -c 2 -n synth pinknoise band -n 2500 4000 tremolo 0.03 5 reverb 20 gain -l 10
-end
