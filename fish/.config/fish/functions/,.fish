3e2f3916ffd5a2277f01a5e5429554c1f15cc779
Sahil Bajaj <sahil29@gmail.com>
2016-07-23 22:27:51 +0800

    [fish] Hide commajump store file

--- fish/.config/fish/functions/,.fish
+++ fish/.config/fish/functions/,.fish
@@ -1,4 +1,4 @@
 function , --description 'Commajump'
-	cd (grep $argv ~/commajump | grep -v (pwd)\$ | uniq -c | sort -nr | head -1 | cut -d' ' -f5)
+	cd (grep $argv ~/.commajump | grep -v (pwd)\$ | uniq -c | sort -nr | head -1 | cut -d' ' -f5)
 	# cd (cat ~/Library/autojump/autojump.txt | grep $argv | grep -v (pwd)\$ | sort -r | head -1 | cut -f 2)
 end
--- fish/.config/fish/functions/__commajump_add.fish
+++ fish/.config/fish/functions/__commajump_add.fish
@@ -1,4 +1,4 @@
 function __commajump_add --on-variable PWD
 	test $PWD = $HOME; and return
-	echo $PWD >> ~/commajump
+	echo $PWD >> ~/.commajump
 end
