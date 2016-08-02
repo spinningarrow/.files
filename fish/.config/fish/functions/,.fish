026635892444fb0b1a418a3253e1f03d7f5fe539
Sahil Bajaj <sahil29@gmail.com>
2016-08-03 01:16:11 +0800

    [fish/commajump] Favour directory names containing the search term
    If a directory doesn't match the search term, fall back to a path that
    does

--- fish/.config/fish/functions/,.fish
+++ fish/.config/fish/functions/,.fish
@@ -1,4 +1,10 @@
 function , --description 'Commajump'
-	cd (grep -i $argv ~/.commajump | grep -v (pwd)\$ | uniq -c | sort -nr | head -1 | cut -d' ' -f5)
-	# cd (cat ~/Library/autojump/autojump.txt | grep $argv | grep -v (pwd)\$ | sort -r | head -1 | cut -f 2)
+	set found_paths (grep -i $argv ~/.commajump | grep -v (pwd)\$)
+	set found_folders (printf '%s\n' $found_paths | xargs basename | grep -in $argv)
+
+	if test (count $found_folders) -ne 0
+		set found_paths $found_paths[(printf '%s\n' $found_folders | cut -d':' -f1 | paste -d' ' -s -)]
+	end
+
+	cd (printf '%s\n' $found_paths | uniq -c | sort -nr | head -1 | tr -s ' ' | cut -d' ' -f3)
 end
