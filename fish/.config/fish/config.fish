fd397685c7663224b70488b0ddd342ed24e9a316
sahil <sahil29@gmail.com>
2016-08-19 21:24:57 +0530

    [brew] Don't keep checking for updates all the time

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,7 @@ set __fish_git_prompt_show_informative_status 'yes'
 # Set program options
 set -x GREP_OPTIONS '--color=auto'
 set -x LESS '-R'
+set -x HOMEBREW_AUTO_UPDATE_SECS 86400
 
 # Set environment variables
 set -x JAVA_HOME (/usr/libexec/java_home)
