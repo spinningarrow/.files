4bea3c9e4ebd79bcd96e5413123d3b7f1c2ff9fb
Sahil Bajaj <sahil29@gmail.com>
2017-03-05 20:53:52 +0800

    [fish] Update commajump

--- fish/.config/fish/functions/,.fish
+++ fish/.config/fish/functions/,.fish
@@ -6,7 +6,7 @@ function , --description 'Commajump'
 		set found_paths $found_paths[(printf '%s\n' $found_folders | cut -d':' -f1 | paste -d' ' -s -)]
 	end
 
-	set found_directory (printf '%s\n' $found_paths | uniq -c | sort -nr | head -1 | tr -s ' ' | cut -d' ' -f3-)
+	set found_directory (printf '%s\n' $found_paths | uniq -c | sort -nr | head -1 | xargs | tr -s ' ' | cut -d' ' -f2-)
 
 	if not test -z $found_directory; and not test -d $found_directory
 		grep -v \^$found_directory\$ ~/.commajump > ~/.commajump.temp; mv ~/.commajump{.temp,}; and echo Nonexistent path $found_directory pruned.
