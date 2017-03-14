49f9e1edebc00bd692537b1f209e41c3c0520f8c
Sahil Bajaj <sahil29@gmail.com>
2017-03-14 14:10:16 +0800

    [fish] Add number of suspended jobs to prompt
    - Also re-indented to follow my typical indentation style

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,24 +1,30 @@
 function fish_prompt --description 'Write out the prompt'
-  # Set custom color variables
-  set -l username_color fdd8aa
-  set -l prompt_color 8cd6ff
+	# Set custom color variables
+	set -l username_color fdd8aa
+	set -l prompt_color 8cd6ff
 
-  set -l last_status $status
+	set -l last_status $status
 
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (pwd | sed -e "s|$HOME|~|")
-  set_color normal
+	# PWD
+	set_color $fish_color_cwd
+	echo -n (pwd | sed -e "s|$HOME|~|")
+	set_color normal
 
-  __fish_git_prompt
-  echo
+	__fish_git_prompt
 
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  else
-    set_color $prompt_color
-  end
+	set jobs_count (jobs | wc -l | xargs)
+	if test $jobs_count != '0'
+		printf ' (↩ %s)' $jobs_count
+	end
 
-  echo -n '∫ '
-  set_color normal
+	echo
+
+	if not test $last_status -eq 0
+		set_color $fish_color_error
+	else
+		set_color $prompt_color
+	end
+
+	echo -n '∫ '
+	set_color normal
 end
