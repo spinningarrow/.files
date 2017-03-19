e276197ffbdf93870c0d06c779364ffac5e9bb27
Sahil Bajaj <sahil29@gmail.com>
2017-03-19 11:30:02 +0800

    [hammerspoon] Automatically reload config file when changed

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -100,3 +100,18 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-", function()
   f.h = f.h - 50
   win:setFrame(f)
 end)
+
+-- Reload config when config file changes
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
