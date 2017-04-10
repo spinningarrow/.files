bc110c9f62cf8baa6031ef3a5691b3b59bef045f
Sahil Bajaj <sahil29@gmail.com>
2017-04-10 12:24:11 +0800

    [lein] Add kibit

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[nightlight/lein-nightlight "1.6.3"]
                   [lein-auto "0.1.3"]
-                  [lein-ancient "0.6.10"]]}}
+                  [lein-ancient "0.6.10"]
+                  [lein-kibit "0.1.3"]]}}
