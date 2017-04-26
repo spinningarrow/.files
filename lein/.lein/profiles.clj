3d78294004652a3bfc2bbe8b4c98fc393d4b3e87
Sahil Bajaj <sahil29@gmail.com>
2017-04-27 00:00:08 +0800

    [lein] Enable ultra for a better REPL

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[nightlight/lein-nightlight "1.6.3"]
                   [lein-auto "0.1.3"]
                   [lein-ancient "0.6.10"]
-                  [lein-kibit "0.1.3"]]}}
+                  [lein-kibit "0.1.3"]
+                  [venantius/ultra "0.5.1"]]}}
