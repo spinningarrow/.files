5dde977af4c5f7298567fb620eca89b88f562532
Sahil Bajaj <sahil29@gmail.com>
2017-07-27 14:42:46 +0800

    [hammerspoon] Move window movement to its own module

---
+++ hammerspoon/.hammerspoon/windows.lua
@@ -0,0 +1,108 @@
+local module = {}
+
+module.init = function()
+	hs.window.animationDuration = 0
+
+	local oldIdXYWH = {}
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+	  local screen = win:screen()
+	  local max = screen:frame()
+
+	  oldIdXYWH = {win:id(), f.x, f.y, f.w, f.h}
+
+	  f.x = max.x
+	  f.y = max.y
+	  f.w = max.w / 2
+	  f.h = max.h
+	  win:setFrame(f)
+	end)
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+	  local screen = win:screen()
+	  local max = screen:frame()
+
+	  oldIdXYWH = {win:id(), f.x, f.y, f.w, f.h}
+
+	  f.x = max.w / 2
+	  f.y = max.y
+	  f.w = max.w / 2
+	  f.h = max.h
+	  win:setFrame(f)
+	end)
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+	  local screen = win:screen()
+	  local max = screen:frame()
+
+	  oldIdXYWH = {win:id(), f.x, f.y, f.w, f.h}
+
+	  f.x = max.x
+	  f.y = max.y
+	  f.w = max.w
+	  f.h = max.h
+	  win:setFrame(f)
+	end)
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+	  local screen = win:screen()
+	  local max = screen:frame()
+
+	  oldIdXYWH = {win:id(), f.x, f.y, f.w, f.h}
+
+	  f.x = max.x + (max.w / 2 - f.w / 2)
+	  f.y = max.y + (max.h / 2 - f.h / 2)
+	  win:setFrame(f)
+	end)
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+
+	  if win:id() ~= oldIdXYWH[1] then
+		  return
+	  end
+
+	  f.x = oldIdXYWH[2]
+	  f.y = oldIdXYWH[3]
+	  f.w = oldIdXYWH[4]
+	  f.h = oldIdXYWH[5]
+	  win:setFrame(f)
+	end)
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "=", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+
+	  oldIdXYWH = {win:id(), f.x, f.y, f.w, f.h}
+
+	  f.x = f.x - 25
+	  f.y = f.y - 25
+	  f.w = f.w + 50
+	  f.h = f.h + 50
+	  win:setFrame(f)
+	end)
+
+	hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-", function()
+	  local win = hs.window.focusedWindow()
+	  local f = win:frame()
+
+	  oldIdXYWH = {win:id(), f.x, f.y, f.w, f.h}
+
+	  f.x = f.x + 25
+	  f.y = f.y + 25
+	  f.w = f.w - 50
+	  f.h = f.h - 50
+	  win:setFrame(f)
+	end)
+end
+
+return module
