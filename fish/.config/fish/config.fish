b976914f15e92b42bb67dd4526826810109b0baf
Sahil Bajaj <sahil29@gmail.com>
2017-08-21 23:18:37 +0800

    Remove unused configs from fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,10 +7,6 @@ source $HOME/.config/fish/abbreviations.fish
 # Initialize commajump
 source $HOME/.config/fish/functions/__commajump_add.fish
 
-# Activate chruby
-[ -f /usr/local/share/chruby/chruby.fish ]; and source /usr/local/share/chruby/chruby.fish
-[ -f /usr/local/share/chruby/auto.fish ]; and source /usr/local/share/chruby/auto.fish
-
 # Git prompt options
 set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_show_informative_status 'yes'
