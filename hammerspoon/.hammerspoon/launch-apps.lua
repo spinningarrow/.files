0b66154ae2b13e1efd86bc776bcd73ba1ad65e6a
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-12 16:39:30 +0800

    Create reusable function for launching apps
    Switched to bundleID because the name for iTerm is 'iTerm2' but it needs
    to be `open()`ed with 'iTerm' (dunno why)

--- hammerspoon/.hammerspoon/launch-apps.lua
+++ hammerspoon/.hammerspoon/launch-apps.lua
@@ -1,25 +1,20 @@
 local module = {}
 
+appLauncher = function (bundleID)
+	return function()
+		local focusedApp = hs.application.frontmostApplication()
+
+		if focusedApp:bundleID() == bundleID then
+			focusedApp:hide()
+		else
+			hs.application.open(bundleID)
+		end
+	end
+end
+
 module.init = function()
-	hs.hotkey.bind({"cmd", "shift"}, "space", function()
-
-		local focusedApp = hs.application.frontmostApplication()
-		if focusedApp:name() == 'iTerm2' then
-			focusedApp:hide()
-		else
-			hs.application.open("iTerm")
-		end
-	end)
-
-	hs.hotkey.bind({'ctrl', 'alt', 'shift'}, 'space', function()
-
-		local focusedApp = hs.application.frontmostApplication()
-		if focusedApp:name() == 'Dynalist' then
-			focusedApp:hide()
-		else
-			hs.application.open('Dynalist')
-		end
-	end)
+	hs.hotkey.bind({'cmd', 'shift'}, 'space', appLauncher('com.googlecode.iterm2'))
+	hs.hotkey.bind({'ctrl', 'alt', 'shift'}, 'space', appLauncher('io.dynalist'))
 end
 
 return module
