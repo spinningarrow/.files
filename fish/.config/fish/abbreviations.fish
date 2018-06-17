7610457af714b2f579ac359be98069b968ec0439
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-17 20:34:19 +0800

    Remove most git aliases in favour of fish abbreviations

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -4,10 +4,24 @@ abbr -a b 'brew'
 abbr -a c 'nix-shell -p clojure --command "clojure -A:rebel -A:cheshire"'
 abbr -a dl 'wget -r -l 1 -np -nd'
 abbr -a fn 'find . -iname'
+
 abbr -a g git
+abbr -a ga git add
+abbr -a gap git add --patch
+abbr -a gb git branch
+abbr -a gc git commit --verbose
+abbr -a gco git checkout
 abbr -a gd git diff
+abbr -a gdc git diff --color-words
+abbr -a gds git diff --staged
 abbr -a gf git fetch
+abbr -a gl git log --graph --decorate
+abbr -a gl1 git log -1
+abbr -a gll git log \'@\{u\}..\'
+abbr -a glr git log \'..@\{u\}\'
+abbr -a gr git remote
 abbr -a gs git status -sb
+
 abbr -a l 'ls -lahp'
 abbr -a lx 'less -X'
 abbr -a o 'open .'
