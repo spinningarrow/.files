3e036cc6f5ce559697ee957399a086f6d2c4216f
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-21 16:27:27 +0800

    Fix keyboard watcher
    It wasn't actually quitting Karabiner properly. Got this workaround from
    https://github.com/tekezo/Karabiner-Elements/issues/1001

--- hammerspoon/.hammerspoon/keyboard-watcher.lua
+++ hammerspoon/.hammerspoon/keyboard-watcher.lua
@@ -4,10 +4,13 @@ local keyboardWatcher = hs.usb.watcher.new(function (event)
 	if event.productName ~= 'DZ60' then
 		return
 	elseif event.eventType == 'added' then
-		hs.application.open('org.pqrs.Karabiner-Menu')
+		local app = hs.application.open('org.pqrs.Karabiner-Elements.Preferences')
+		app:kill()
 	else
 		local app = hs.application.find('org.pqrs.Karabiner-Menu')
 		if app then app:kill() end
+		hs.execute('launchctl bootout gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server')
+		hs.execute('launchctl disable gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server')
 	end
 end)
 
