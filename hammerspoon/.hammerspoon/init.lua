90b85b0c2522e9460481e2bb70958542f9afef60
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-17 14:45:31 +0800

    Remove now-playing functionality
    Spotify in the notification centre sidebar is good enough

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,9 +4,6 @@ windows.init()
 local configReloader = require('config-reloader')
 configReloader.init()
 
-local nowPlaying = require('now-playing')
-nowPlaying.init()
-
 local displaySleep = require('display-sleep')
 displaySleep.init()
 
