50ffaa0473ff18a5e9e75c902dc8dac80a4763eb
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-08-01 19:30:22 +0800

    Revert "Launch Kitty instead of iTerm"
    This reverts commit 379d04d7f7aa3ba133f6be81ed2551e88376f445.

--- hammerspoon/.hammerspoon/launch-apps.lua
+++ hammerspoon/.hammerspoon/launch-apps.lua
@@ -13,7 +13,7 @@ function appLauncher(bundleID)
 end
 
 module.init = function()
-	hs.hotkey.bind({'cmd', 'shift'}, 'space', appLauncher('net.kovidgoyal.kitty'))
+	hs.hotkey.bind({'cmd', 'shift'}, 'space', appLauncher('com.googlecode.iterm2'))
 	hs.hotkey.bind({'ctrl', 'alt', 'shift'}, 'space', appLauncher('io.dynalist'))
 end
 
