5128d31b7609cfe952a29a55fd2fef825c6d9023
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2019-05-07 15:28:20 +0200

    Fix bad output when job fails

--- hammerspoon/.hammerspoon/pipelines.fish
+++ hammerspoon/.hammerspoon/pipelines.fish
@@ -2,6 +2,6 @@
 
 fly -t $argv pipelines | cut -d' ' -f1 | while read p
 	echo -n $p
-	fly -t $argv jobs -p $p | grep failed
+	fly -t $argv jobs -p $p | grep failed > /dev/null
 	echo '' $status
 end
