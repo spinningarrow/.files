aee2ca6fcb76c7abc6dd0feccf5fd2125b1bb700
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2020-06-02 14:00:53 +0200

    Clean up hammerspoon modules

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,7 +1,6 @@
-require('windows').init()
+hs.loadSpoon('ControlEscape'):start()
+
 require('config-reloader').init()
 require('launch-apps').init()
--- require('keyboard-watcher').init()
 require('wallpaper').init()
-require('gitlab').init('')
-hs.loadSpoon('ControlEscape'):start()
+require('windows').init()
