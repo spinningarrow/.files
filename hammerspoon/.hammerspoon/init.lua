cba12b73707306f2964e5f76f9c75d1bafbf46d2
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2021-01-12 18:48:00 +0100

    Use SpoonInstall and add some spoons
    - ReloadConfiguration instead of config-reloader
    - DeepLTranslate
    - WindowGrid for grid management

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,6 +1,24 @@
-hs.loadSpoon('ControlEscape'):start()
-
-require('config-reloader').init()
 require('launch-apps').init()
 require('wallpaper').init()
 require('windows').init()
+
+hs.loadSpoon('SpoonInstall')
+hs.loadSpoon('ControlEscape'):start()
+
+spoon.SpoonInstall:andUse('ReloadConfiguration', { start = true })
+
+spoon.SpoonInstall:andUse("DeepLTranslate", {
+	hotkeys = {
+		translate = { {'ctrl', 'alt', 'shift'}, 'D' }
+	}
+})
+
+spoon.SpoonInstall:andUse('WindowGrid', {
+	config = {
+		gridGeometries = { { '4x4' } }
+	},
+	hotkeys = {
+		show_grid = { {'ctrl', 'alt', 'shift'}, 'G' }
+	},
+	start = true
+})
