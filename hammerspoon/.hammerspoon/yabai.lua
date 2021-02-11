a011f32756b6250f2a1fb1ee06b9738ea44deba5
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2021-02-11 11:41:33 +0100

    Invoke yabai in a platform-independent way
    - env does not work for some reason
    - zsh should work regardless of homebrew location

--- hammerspoon/.hammerspoon/yabai.lua
+++ hammerspoon/.hammerspoon/yabai.lua
@@ -2,7 +2,7 @@ local module = {}
 
 module.init = function()
 	hs.hotkey.bind({'ctrl', 'alt', 'shift'}, 'y', function()
-		local t = hs.task.new('/usr/local/bin/yabai', function() end, {'-m', 'window', '--toggle', 'float'})
+		local t = hs.task.new('/bin/zsh', function() end, {'-lc', 'yabai -m window --toggle float'})
 		t:start()
 	end)
 end
