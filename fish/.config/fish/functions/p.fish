6d9f1eec45334d316ee55cf6805539a4e3e3d541
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2022-01-02 17:24:00 +0100

    Use pass with `p` instead of 1password

--- fish/.config/fish/functions/p.fish
+++ fish/.config/fish/functions/p.fish
@@ -1,5 +1,4 @@
 # Defined in - @ line 1
 function p
-	op get item --vault=$argv[1] $argv[2] | jq -r '.details.fields[] | select(.designation=="password").value' | tr -d '
-' | pbcopy
+	pass show (gopass ls --flat | fzf) | fzf --bind "enter:execute(echo -n {} | pbcopy)"
 end
