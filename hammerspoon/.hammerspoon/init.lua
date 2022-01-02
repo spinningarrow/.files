99f35acc20b499c751af4904f1c6b0d9c8fe837c
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2022-01-02 17:24:46 +0100

    Automatically toggle default browser

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,8 +5,10 @@ require('yabai').init()
 
 hs.loadSpoon('SpoonInstall')
 hs.loadSpoon('ControlEscape'):start()
+ud = hs.loadSpoon('URLDispatcher')
 
 spoon.SpoonInstall:andUse('ReloadConfiguration', { start = true })
+spoon.SpoonInstall:andUse('URLDispatcher', { start = true })
 
 spoon.SpoonInstall:andUse("DeepLTranslate", {
 	hotkeys = {
@@ -24,3 +26,21 @@ spoon.SpoonInstall:andUse('WindowGrid', {
 	},
 	start = true
 })
+
+local w = hs.application.watcher.new(function (name, event, app)
+	print('application watcher triggered', name)
+
+	if (name ~= 'Safari') then
+		return
+	end
+
+	if (event == hs.application.watcher.hidden) then
+		ud.default_handler = 'com.google.Chrome'
+		print('setting handler to chrome', name, event)
+	else
+		ud.default_handler = 'com.apple.Safari'
+		print('setting handler to safari', name, event)
+	end
+end)
+
+w:start()
