dd6512698a3ccc7ae3357569525940b0e8e3f658
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2022-01-04 17:33:54 +0100

    Use gopass for CLI password manager

--- fish/.config/fish/functions/p.fish
+++ fish/.config/fish/functions/p.fish
@@ -1,4 +1,3 @@
-# Defined in - @ line 1
 function p
-	pass show (gopass ls --flat | fzf) | fzf --bind "enter:execute(echo -n {} | pbcopy)"
+    gopass ls --flat | fzf-tmux --bind "enter:execute(gopass show -n -u {} | fzf --tac --no-sort | xargs echo -n | pbcopy < /dev/tty > /dev/tty 2>&1)"
 end
