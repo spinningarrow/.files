9e5eee082c508b8a76c79187b0b81fc43de62bcc
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2023-05-25 20:01:15 +0200

    Clean up init.lua

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -6,17 +6,8 @@ require('spt').init()
 
 hs.loadSpoon('SpoonInstall')
 hs.loadSpoon('ControlEscape'):start()
-ud = hs.loadSpoon('URLDispatcher')
 
 spoon.SpoonInstall:andUse('ReloadConfiguration', { start = true })
-spoon.SpoonInstall:andUse('URLDispatcher', { start = true })
-
-spoon.SpoonInstall:andUse("DeepLTranslate", {
-	hotkeys = {
-		translate = { {'ctrl', 'alt', 'shift'}, 'D' }
-	}
-})
-
 
 spoon.SpoonInstall:andUse('WindowGrid', {
 	config = {
@@ -27,21 +18,3 @@ spoon.SpoonInstall:andUse('WindowGrid', {
 	},
 	start = true
 })
-
-local w = hs.application.watcher.new(function (name, event, app)
-	print('application watcher triggered', name)
-
-	if (name ~= 'Safari') then
-		return
-	end
-
-	if (event == hs.application.watcher.hidden) then
-		ud.default_handler = 'com.google.Chrome'
-		print('setting handler to chrome', name, event)
-	else
-		ud.default_handler = 'com.apple.Safari'
-		print('setting handler to safari', name, event)
-	end
-end)
-
-w:start()
