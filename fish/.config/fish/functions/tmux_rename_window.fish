9326b8261a9950d6b9482648ac0c3cf8b94d3118
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2023-05-25 19:50:15 +0200

    Name tmux window using git repo if possible
    So that subdirs keep the repo name instead of the dir name

--- fish/.config/fish/functions/tmux_rename_window.fish
+++ fish/.config/fish/functions/tmux_rename_window.fish
@@ -1,3 +1,3 @@
 function tmux_rename_window --on-variable PWD
-	tmux info >/dev/null 2>&1 ; and tmux rename-window (basename $PWD)
+	tmux info >/dev/null 2>&1 ; and tmux rename-window (basename (git config --get remote.origin.url; or pwd))
 end
