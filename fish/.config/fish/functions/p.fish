3736201a3b339ad6374fdb5558ae52731215a1cd
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2023-08-18 14:56:12 +0200

    Simplify pass (p) function

--- fish/.config/fish/functions/p.fish
+++ fish/.config/fish/functions/p.fish
@@ -1,4 +1,4 @@
 function p
     pass show personal/dummy
-    find ~/.password-store -name '*.gpg' | sed 's:'$HOME/.password-store/'::' | sed 's/.gpg//' | fzf-tmux --bind "enter:execute(pass show {} | fzf --tac --no-sort | xargs echo -n | pbcopy < /dev/tty > /dev/tty 2>&1)"
+    find ~/.password-store -name '*.gpg' | sed -E 's:'$HOME/.password-store/'(.+).gpg:\1:' | fzf-tmux --bind "enter:execute(pass show {} | fzf --tac --no-sort | xargs echo -n | pbcopy < /dev/tty > /dev/tty 2>&1)"
 end
