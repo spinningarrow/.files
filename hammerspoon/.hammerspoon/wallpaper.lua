d43db214c26dbc66b5f1103bad365fabb5098fee
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2025-12-08 15:32:45 +0100

    Clean up old hammerspoon files

--- hammerspoon/.hammerspoon/wallpaper.lua
+++
@@ -1,9 +0,0 @@
-local module = {}
-
-module.init = function()
-	hs.hotkey.bind({'alt', 'ctrl', 'shift'}, 'w', function()
-		local success, result, err = hs.osascript.applescriptFromFile('./scripts/wallpaper.applescript')
-	end)
-end
-
-return module
