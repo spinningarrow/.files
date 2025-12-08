05bfa97bcaa844d5baaabe3098917249aa28752e
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2025-12-08 15:29:07 +0100

    Clean up init.lua, move to main.fnl
    Re-enabled clipboard history, Tahoe sucks

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,30 +5,6 @@ hs.loadSpoon('ControlEscape'):start()
 
 spoon.SpoonInstall:andUse('ReloadConfiguration', { start = true })
 
-spoon.SpoonInstall:andUse('ClipboardTool', {
-	start = true,
-	hotkeys = {
-		toggle_clipboard = { {'ctrl', 'alt', 'shift'}, 'B' }
-	},
-	config = {
-		display_max_length = 40,
-		frequency = 1,
-		show_copied_alert = false,
-		show_in_menubar = false,
-		hist_size = 10
-	}
-})
-
-spoon.SpoonInstall:andUse('WindowGrid', {
-	config = {
-		gridGeometries = { { '2x2' } }
-	},
-	hotkeys = {
-		show_grid = { {'ctrl', 'alt', 'shift'}, 'G' }
-	},
-	start = true
-})
-
 -- Fennel support
 package.path =
 package.path .. ";" .. os.getenv("HOME") .. "/.hammerspoon/?.lua"
